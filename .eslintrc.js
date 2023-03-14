b05cc84fa1b99edc6aaf1bc18e3ce2973762bdef
wdssmq <wdssmq@qq.com>
2023-03-14 11:05:59 +0000

    base. init config

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+    'env': {
+        'es2021': true,
+        'node': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended'
+    ],
+    'overrides': [
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
+    },
+    'plugins': [
+        '@typescript-eslint'
+    ],
+    'rules': {
+        'indent': [
+            'error',
+            4,
+            { 'SwitchCase': 1 }
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'never'
+        ],
+        'no-unused-vars': [
+            'warn',
+            { 'args': 'none' }
+        ],
+    }
+}
